f31ef73e9dc20752355c4a020dd705aa123c34a5
xxcaioken <cwkormives@gmail.com>
2022-05-25 22:00:09 -0300

    Add files via upload

---
+++ oi.py
@@ -0,0 +1,109 @@
+num1 = int(input("insira dois numeros positivos ate 10000"))
+contador = int(input("insira o contador"))
+num2 = 0
+num3 = 0
+num4 = 0
+num5 = 0
+if 10>num1>0:
+    print(num1)
+    num2 = 1
+    print(num2, num1)
+    if contador >= 2:
+        num3 = 1
+        print(num3,num2,num1)
+        if contador >= 3:        
+            num4 =2
+            print(num4,num3,num2,num1)
+            if contador >=4:
+                num5 =1
+                print(num5,num4,num3,num2,num1)
+                if contador >=5:
+                    num6 =1
+                    print(num6,num5,num4,num3,num2,num1)
+                    if contador >=6:
+                        num7 =2
+                        print(num7,num5,num4,num3,num2,num1)
+                        if contador >=7:
+                            num8 =1
+                            print(num8,num7,num5,num4,num3,num2,num1)
+                            if contador >=7:
+                                num9 =1
+                                print(num9,num8,num7,num5,num4,num3,num2,num1)
+
+elif 100>num1>=10:
+        print(num1)
+        num2 = 2
+        print(num2, num1)
+        if contador >= 2:
+            num3 = 1
+            print(num3,num2,num1)
+            if contador >= 3:        
+                num4 =1
+                print(num4,num3,num2,num1)
+                if contador >=4:
+                    num5 =2
+                    print(num5,num4,num3,num2,num1)
+                    if contador >=5:
+                        num6 =1
+                        print(num6,num5,num4,num3,num2,num1)
+                        if contador >=6:
+                            num7 =1
+                            print(num7,num5,num4,num3,num2,num1)
+                            if contador >=7:
+                                num8 =2
+                                print(num8,num7,num5,num4,num3,num2,num1)
+                                if contador >=7:
+                                    num9 =1
+                                    print(num9,num8,num7,num5,num4,num3,num2,num1)
+elif 1000>num1>=100:
+        print(num1)
+        num2 = 3
+        print(num2, num1)
+        if contador >= 2:
+            num3 = 1
+            print(num3,num2,num1)
+            if contador >= 3:        
+                num4 =1
+                print(num4,num3,num2,num1)
+                if contador >=4:
+                    num5 =2
+                    print(num5,num4,num3,num2,num1)
+                    if contador >=5:
+                        num6 =1
+                        print(num6,num5,num4,num3,num2,num1)
+                        if contador >=6:
+                            num7 =1
+                            print(num7,num5,num4,num3,num2,num1)
+                            if contador >=7:
+                                num8 =2
+                                print(num8,num7,num5,num4,num3,num2,num1)
+                                if contador >=7:
+                                    num9 =1
+                                    print(num9,num8,num7,num5,num4,num3,num2,num1)
+
+elif 10000>num1>=1000:
+        print(num1)
+        num2 = 4
+        print(num2, num1)
+        if contador >= 2:
+            num3 = 1
+            print(num3,num2,num1)
+            if contador >= 3:        
+                num4 =1
+                print(num4,num3,num2,num1)
+                if contador >=4:
+                    num5 =2
+                    print(num5,num4,num3,num2,num1)
+                    if contador >=5:
+                        num6 =1
+                        print(num6,num5,num4,num3,num2,num1)
+                        if contador >=6:
+                            num7 =1
+                            print(num7,num5,num4,num3,num2,num1)
+                            if contador >=7:
+                                num8 =2
+                                print(num8,num7,num5,num4,num3,num2,num1)
+                                if contador >=7:
+                                    num9 =1
+                                    print(num9,num8,num7,num5,num4,num3,num2,num1)
+        
